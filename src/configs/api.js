8dacacd966800573479cdc6cacfe0d6614067a60
Affan Raza <razaaffan08@gmail.com>
2025-11-05 00:19:52 +0530

    APi updated

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,11 +1,11 @@
-
 import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL ||
-    'http://localhost:3000/api',
-  withCredentials: true, // ✅ allows sending cookies / auth headers if needed
+    import.meta.env.VITE_API_URL
+      ? `${import.meta.env.VITE_API_URL}/api`
+      : "http://localhost:3000/api",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
